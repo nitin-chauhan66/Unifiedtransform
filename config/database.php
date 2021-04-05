6d9e459c9011a22a94f94832d44b7681ed33c5f3
nitinsfdc <nvch66@gmail.com>
2021-04-06 01:09:46 +0530

    env changed

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $_SERVER['DB_HOST'],
+            'port' => $_SERVER['DB_PORT'],
+            'database' => $_SERVER['DB_DATABASE'],
+            'username' => $_SERVER['DB_USERNAME'],
+            'password' => $_SERVER['DB_PASSWORD'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
