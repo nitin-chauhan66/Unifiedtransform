20360c3c32038e2a820e09e8b81182e8874b77cb
nitinsfdc <nvch66@gmail.com>
2021-04-25 13:27:50 +0530

    added mysql config

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => getenv('DB_HOST', '127.0.0.1'),
+            'port' => getenv('DB_PORT', '3306'),
+            'database' => getenv('DB_DATABASE', 'forge'),
+            'username' => getenv('DB_USERNAME', 'forge'),
+            'password' => getenv('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
